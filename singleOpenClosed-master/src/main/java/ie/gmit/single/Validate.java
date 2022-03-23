6970fa382e4db6165757ee5fc3cd30eff5a47614
JuniorAJ1 <juniorajala17@outlook.com>
2022-03-23 10:28:07 +0000

    first committ

--- singleOpenClosed-master/src/main/java/ie/gmit/single/Validate.java
+++ singleOpenClosed-master/src/main/java/ie/gmit/single/Validate.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Validate {
 
-    public static boolean checkUser(User user){
+    public boolean checkUser(User user){
         return isValidUser(user);
     }
 
