ec36872ef9d061d6462b483eb2c11a9341a127f4
JuniorAJ1 <juniorajala17@outlook.com>
2022-03-23 10:21:53 +0000

    first committ

---
+++ singleOpenClosed-master/src/main/java/ie/gmit/single/UserController.java
@@ -0,0 +1,34 @@
+package ie.gmit.single;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserController {
+
+    //Handles incoming JSON requests that work on User resource/entity
+
+        //Store used by controller
+        private Store store = new Store();
+
+        //Create a new user
+        public String createUser(String userJson) throws IOException {
+            ObjectMapper mapper = new ObjectMapper();
+
+            User user = mapper.readValue(userJson, User.class);
+
+            if(!Validate.checkUser(user)) {
+                return "ERROR";
+            }
+
+            store.store(user);
+
+            return "SUCCESS";
+        }
+
+
+
+ }
+
